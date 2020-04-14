1eab77410377b2d493b4ebe5ef4418bd63e92771
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-14 21:28:52 +0600

    introduction and print special character by unicode

--- Introduction.py
+++ Introduction.py
@@ -1,1 +1,10 @@
 print("This repository contains online course that I have completed in udemy")
+
+print("special character by unicode")
+
+st= "\u03B3"
+print(st)
+st= "\u03B4"
+print(st)
+st= "\u03B5"
+print(st)
