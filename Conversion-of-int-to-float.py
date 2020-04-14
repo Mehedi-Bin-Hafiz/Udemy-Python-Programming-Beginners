080a158ea55a1dcdb72670241ad5205ef1242f75
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-14 21:55:15 +0600

    in this file instructor discuss about rules and regulation of defining variable

---
+++ Conversion-of-int-to-float.py
@@ -0,0 +1,8 @@
+v=3.1416
+print(int(v))
+m=12
+print(float(m))
+
+#exponential
+ex=3493.1232123**2
+print(ex)
---
+++ Rules-of-defining-python.py
@@ -0,0 +1,12 @@
+#key word never be a variable
+print(help("keywords"))
+
+x=2
+print(id(x))
+
+# id=4
+# print(id)
+#
+# print(id(id))
+
+#overriding builtin function is not allowed
