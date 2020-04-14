08beffd8b2a62f8004f18150ced5f4371642a052
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-15 01:05:30 +0600

    simultaneous assignments

---
+++ ArithmaticOperator.py
@@ -0,0 +1,23 @@
+#python automatic detect the type of variable
+
+# v=x**2
+# print(x)
+#NameError: name 'x' is not defined
+
+#simultaneous assignments
+x1,x2,x3=1,2,3
+print(x1,x2,x3)
+
+#swaping the value of variable using simultaneous assignments
+
+x1,x2=x2,x1
+print("x1=",x1)
+print("x2=",x2)
+
+
+# floor division
+
+x=5//2
+print(x)
+
+#it remove floating point
