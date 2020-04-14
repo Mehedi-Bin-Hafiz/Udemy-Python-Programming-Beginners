782dff7653da380dc9bf81920077d47eced914d2
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-14 21:40:28 +0600

    introduction to numeric data type. and pointer is also discussed

---
+++ Numeric-datatype.py
@@ -0,0 +1,9 @@
+#in python variable are pointers if we declare
+a=2
+# a point a memory location that contains the value 2
+# we can see the changes of memory location by id funcion
+print("Id of 2",id(a))
+a="mehedi"
+print("Id of mehedi",id(a))
+
+
