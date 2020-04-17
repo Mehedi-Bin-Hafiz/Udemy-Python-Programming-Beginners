eeb12733d69d2b9ad6159be0abbab8c54ffcaadf
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-17 18:04:00 +0600

    discussion about math module

---
+++ CompundOperatorAndOperatorPrecedence.py
@@ -0,0 +1,21 @@
+#compund operator
+x=10
+y=3
+x%=y
+print(x)
+
+x += 10
+print(x)
+
+""" Notice there is no space between + and ="""
+
+#operator precedence
+#for precedence python follow BODMAS rule if there are same precedence operator are coming then it follow left associative rule
+
+v=2+6/3
+print(v)
+v=2* 3/3
+print(v)
+
+
+
---
+++ MathModule.py
@@ -0,0 +1,31 @@
+import math
+x=math.sqrt(3)
+print(x)
+
+# constant pi
+print(math.pi)
+
+#factiorial
+print(math.factorial(5))
+
+#ceil method return next integer bigger than the given number
+print(math.ceil(-2.99))
+print(math.ceil(3.12))
+#floor method give next integer smaller than the given number
+print(math.floor(-2.12))
+print(math.floor(3.99))
+# fabs returns the absolute value of given number
+print(math.fabs(-3.12))
+#round method returns bigger value than given number if floating point is <=50
+print(round(3.49))
+
+#practioc part
+a='10'
+b=10
+a=int(a)+b
+print(a)
+
+
+d=1
+d -= (-1)
+print(d)
