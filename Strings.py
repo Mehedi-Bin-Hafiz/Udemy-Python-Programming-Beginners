3a438dc8df0c495dfc43e6f523e1933182bab73e
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-17 22:02:35 +0600

    Basic about strings

---
+++ Strings.py
@@ -0,0 +1,20 @@
+print("{:.2f}".format(3.1416))
+print(type("{:.2f}".format(3.1416)))
+
+#in python string is a sequence of unicode characters
+a='Happy'
+print(a[2])
+#last item of strings
+print(a[-1])
+
+print("It's ok")
+
+#convert number to string
+x=10
+print(type(x))
+
+x=3.1214
+
+print(int(x))
+x="3.1214"
+print(float(x))  #Note this type of string cant not be type casted to int. But it can be float
