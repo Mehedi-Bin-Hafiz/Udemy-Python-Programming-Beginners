1f6d5ce15dbadb843cfbfce432f89448bb383bf8
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-17 20:26:49 +0600

    discussion about Print function and its parameter

---
+++ PrintFunction.py
@@ -0,0 +1,9 @@
+#seperator
+print("mehedi","hasan", sep=':')
+
+#end parameter : by default each print method contains new line at the end of the file. But we can change it by end parameter
+un=u"\u03F0"
+for  i in range(1,10):
+    print(end='\n')
+    for j in range(0,i):
+        print(un,end='')
