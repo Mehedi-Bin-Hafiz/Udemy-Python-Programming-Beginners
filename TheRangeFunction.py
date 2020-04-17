8f1f750db1761c21f2b5cd816dd3fa4e770021b9
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-17 20:48:04 +0600

    details about range function

---
+++ TheRangeFunction.py
@@ -0,0 +1,7 @@
+#range(start,stop,step)
+#start= lower limit; stop=upper limit ; step = interval
+
+#print descending order
+
+for i in range(10,1,-1): #-1 represent upper to lower by difference 1
+    print(i)
