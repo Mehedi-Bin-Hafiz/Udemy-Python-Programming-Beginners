216936b0d9fa95dc8b67da35c72c816e45ddf0a2
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-17 20:40:26 +0600

    check use of unicode

--- PrintFunction.py
+++ PrintFunction.py
@@ -2,7 +2,7 @@
 print("mehedi","hasan", sep=':')
 
 #end parameter : by default each print method contains new line at the end of the file. But we can change it by end parameter
-un=u"\u03F0"
+un=u"\u265E"
 for  i in range(1,10):
     print(end='\n')
     for j in range(0,i):
