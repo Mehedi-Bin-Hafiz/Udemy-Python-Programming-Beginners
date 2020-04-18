63ef7aae548fc859b79a38d1273ffa610ad6c912
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-18 11:02:34 +0600

    immutability of string

---
+++ ImmutableString.py
@@ -0,0 +1,13 @@
+f=" mehedi"
+
+#we can not change eny of char form this string because strings are immutable
+l=" hasan"
+
+#concation of string
+full=f+l
+print(full)
+
+# string replication "*" operator is used for replicating string
+
+print(f*5)
+
