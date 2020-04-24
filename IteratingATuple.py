1de60ed04c4ee14f1ebb8e6ea033870489ae9a14
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-24 22:46:15 +0600

    iterating and some operation of tuple

---
+++ IteratingATuple.py
@@ -0,0 +1,18 @@
+# A for loop is use to iterate tuple
+tup = (1,2,3,4,5,6)
+for i in tup:
+    print(i)
+
+#membership operation
+print(5 in tup)
+print(5 not in tup)
+
+tup2 = (1,2,3,4,5,6)
+
+print(tup == tup2)
+
+#identity operator
+
+print(tup is tup2)
+
+
