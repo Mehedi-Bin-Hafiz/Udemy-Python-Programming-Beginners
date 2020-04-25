1f754b48a538c6441e3bb98217022905b5b7e319
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-25 14:19:33 +0600

    introduction to python set

--- Set/PythonSet.py
+++ Set/PythonSet.py
@@ -4,23 +4,41 @@
 # caution : set itself is a mutable but it can not contain mutable data set ten it produce error
 
 # be aware a=  {} represent dictionary
-set ={ 'python', 'mehedi', 'hasan',1,2,1,1,2,3,4,5,5}
+s ={ 'python', 'mehedi', 'hasan',1,2,1,1,2,3,4,5,5}
 
-print(set) #python automatically remove  duplicate value from the set
-
-
-
-
-# set ={ 'python', 'mehedi', 'hasan',1,2,1,1,2,3,4,5,5,[1,2,3,4,5]}
-# print(set)
-
-# set ={ 'python', 'mehedi', 'hasan',1,2,1,1,2,3,4,5,5,{1,2,3,4,5}}
-# print(set)
+print(s)
+#python automatically remove  duplicate value from the set
 
 
-### it produce type error
+x = set(['foo', 'bar', 'baz', 'foo', 'qux'])
+print(x)
+
 
 # but set contains any tuples because tuple is immutable
-set ={ 'python', 'mehedi', 'hasan',1,2,1,1,2,3,4,(5,5)}
-print(set)
+s ={ 'python', 'mehedi', 'hasan',1,2,1,1,2,3,4,(5,5)}
+print(s)
+# s ={ 'python', 'mehedi', 'hasan',[1,2,1,1,2],3,4,(5,5)}
+# print(s) produce type error
+
+#add() method can add only immutable data. as set is a unordered data type so element can be added any index
+
+#add(), clear(), update(), discord(), remove() mehtods
+s.add(12)
+print(s)
+
+s.update([88,99,1,2])
+print(s) # if we want to add two set we can do it by update method
+
+#dicard() method
+
+s.discard(88) # it produce no error if there is no element
+print(s)
+# s.remove(88) # it produce key error if there is no element
+# we can use pop() method to remove 1st element from set
+
+
+
+
+
+
 
