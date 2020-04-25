7698f944352d74c68326233fc426f78658608f49
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-25 21:12:50 +0600

    Introduction to if statement

---
+++ ConditionalStatement/ifStatement.py
@@ -0,0 +1,10 @@
+# it is also known as control flow statement
+if(1):
+    print(True)
+flag=False
+if(flag): # if is executed only the particular condition is true so flag is True then if condition is executed
+    print(True)
+# 0 , list=[],s='' all are evaluated as false so
+if(''):
+    print('never execute')
+
