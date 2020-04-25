b1b2c5c5278a2a622891c2d3a7b59423e88c702d
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-25 21:36:46 +0600

    intro to if-else statement

---
+++ ConditionalStatement/if-elseStatement.py
@@ -0,0 +1,16 @@
+color='green'
+if(color=='red'): #condition is false so it executes else statement
+    print('go')
+else:
+    print('false')
+
+
+#if..elif ...else
+color='green'
+if(color=='red'): #condition is false so it executes else statement
+    print('go')
+elif(color=='green'):
+    print('impossible') #after execute elif and if it is true then after execute if will be terminated
+else:
+    print('false')
+
