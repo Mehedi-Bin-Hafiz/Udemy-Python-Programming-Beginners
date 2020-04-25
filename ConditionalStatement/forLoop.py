8f982ca71698dd14ba7ae931139b36f172989681
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-25 21:37:43 +0600

    for include additional feature that is else block

---
+++ ConditionalStatement/forLoop.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print(i*i/2)
+for i in 'mehedi':
+    print(i)
+
+# we can have ans optional else block associated with the for loop
+# it is executed when all iteration is completed by for loop
+
+for i in range(5):
+    print(i)
+else:
+    print('for loop is executed ')
