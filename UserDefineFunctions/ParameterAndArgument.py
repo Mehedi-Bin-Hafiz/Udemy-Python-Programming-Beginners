0ceea20b0f708d73add78736063c36c3d044dee0
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-04-26 12:10:51 +0600

    introduction to parameter and argument

---
+++ UserDefineFunctions/ParameterAndArgument.py
@@ -0,0 +1,7 @@
+# a parameter or parameter variable is a variable in the function header which receive an argument when the function is called.
+# An argument is nothing but a piece of data passed to the function, when it is called
+
+def add(a,b): # here a and b are parameters
+    return a+b
+m=add(3,4) # here 3 and 4 are argument
+print(m)
