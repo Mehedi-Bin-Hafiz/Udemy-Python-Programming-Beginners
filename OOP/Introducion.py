ef294d41d9bb02fc530b04e4d9d1303d16b87bf9
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-04 23:39:17 +0600

    convention of OOP

---
+++ OOP/Introducion.py
@@ -0,0 +1,13 @@
+# OOP is a concept of object and class
+# there are three widely used programming style procedural, functional, and OOP
+# python support all these style
+# once we create class we can create object of this class and keep it into a variable. it is known as instance
+
+
+# the init method also known as magic or special method. when a function created starting with double __ then it called special method.
+# special method inside class is called firstly and automatically when the class is called
+
+# self.variable is known as instance variable. if we access instance variable within the class then we use self.variable
+
+
+
