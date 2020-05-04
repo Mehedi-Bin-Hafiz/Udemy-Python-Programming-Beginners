b3597929b833bd31aa4960f7ed98e564149c9c5c
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-05 00:14:22 +0600

    All basic information to understand OOP

--- OOP/Introducion.py
+++ OOP/Introducion.py
@@ -9,5 +9,11 @@
 
 # self.variable is known as instance variable. if we access instance variable within the class then we use self.variable
 
+# we can access instance attributes by the name of the instance of this class and within the class we use self parameter
 
 
+# class attributes and instance attributes .
+# basically we declare instance attribute inside the init method
+# basically we declare class attribute after declaration of class
+# instance attribute are different based on instances. but class attribute are same for all
+''' if we want to change class attribute then we can do it by this syntax classname.attributename=value'''
