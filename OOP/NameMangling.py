c57bba8d71c1b1efef6ba4161e5773cb4ff5bb2f
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-14 00:39:57 +0600

    documentation

---
+++ OOP/NameMangling.py
@@ -0,0 +1,5 @@
+#nam korton -->  Name Mangling is to avoid the risk of conflict when designing classes for inheritance.!
+
+# Python interpreter applies name mangling to protect the variable from being overridden in subclasses.
+# The way of name mangling is add double underscore in a variable or function.
+# when we add __ then python automatically mangle it _class__function or variable name.
