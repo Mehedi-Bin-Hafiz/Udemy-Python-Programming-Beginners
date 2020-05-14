ea52b3ed3f4b3a0f47c2285c79d13b9fb8bce97d
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-15 03:09:43 +0600

    intro to package management

---
+++ PackageManagementSystem/pip.py
@@ -0,0 +1,9 @@
+# a package is a collection of individual files and information about those files.
+# The files are packed together as a software collection that provides certain functionality as part of a larger system.
+
+# by package manager we can download package form software repositories and then install it
+# we can find pip by command where pip
+
+# pip install yolk3k
+# it is a package by which we can see install package . also pip freeze do same work
+
---
+++ PackageManagementSystem/vartual_environment 1.py
@@ -0,0 +1,7 @@
+# different application may be needed different version of package. one version can not be supported by another version. in order to maintain version we can use virtual environment.
+# virtual environment can isolate one application environment from another.
+
+# what is vm?
+# vm allow packages to be installed in an isolated location for a particular application, rather than being installed globally.
+# the venv module provides support for creating virtual environments.
+# each virtual environment has its own python binary and can have its own independent set of installed python packages in its site directories.
