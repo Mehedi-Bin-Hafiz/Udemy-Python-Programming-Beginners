98c2b941de02edc182de327dfc45821de25b82b9
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-15 23:52:30 +0600

    Introduction to __name__

---
+++ ModulesAndPackages/Attribute__name__.py
@@ -0,0 +1,10 @@
+print(dir())
+# what is __name__
+print(__name__) #__name__ assign the value __main__
+
+def hello():
+    print("hello world")
+print(dir())
+
+print("__name__ is ",__name__)
+hello()
